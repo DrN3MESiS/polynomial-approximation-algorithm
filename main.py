39b44faf5393c99cda6d7edb28584a5e7275f5e6
Alan Maldonado <business.alanm@gmail.com>
2019-10-23 20:25:24 -0500

    FEATURE: Version 2.0.0 > Added Graphing for the functions

--- main.py
+++ main.py
@@ -3,7 +3,7 @@
 Created on Wed Oct 23 19:16:45 2019
 
 @author: Alan Maldonado
-@version: 1.0.0
+@version: 2.0.0
 """
 
 """
@@ -13,10 +13,14 @@ Output: Polynomic Function
 
 # Libraries
 import sympy as sp
+import numpy as np 
+import matplotlib.pyplot as plt
 import math
 
+
+
 #Default Variables
-x_lim = [-2, 3] #Graph Range
+xlim = [-2, 3] #Graph Range
 
 #Default Functions
 def algorithmStart(f, grad, x0):
@@ -27,6 +31,26 @@ def algorithmStart(f, grad, x0):
         f_der = sp.diff(f_der, 'x')
     p = sp.expand(p)
     print(p)
+    
+    #Graphing
+    x_data = np.linspace(xlim[0], xlim[1], 500)
+    f_vector = sp.lambdify(x,f,'numpy')
+    f_data = f_vector(x_data)
+    
+    P_vector = sp.lambdify(x,p,'numpy')
+    P_data = P_vector(x_data)
+    
+    fig = plt.figure(1)
+    ax = fig.add_subplot(1,1,1)
+    ax.cla()
+    ax.plot(x_data, f_data, color=(0,0,1), lw=4)
+    ax.plot(x_data, P_data, color=(0,1,0), ls='--',lw=4)
+    ax.grid(True)
+    ax.legend(['Function', 'Taylor'])
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    fig.canvas.draw()
+    
 
 ########### Change
 x = sp.Symbol('x') #X is the symbol.
