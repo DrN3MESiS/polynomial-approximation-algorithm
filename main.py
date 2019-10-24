92726d1530871448778b81cd6297fa32ea61276e
Alan Maldonado <business.alanm@gmail.com>
2019-10-23 20:12:43 -0500

    UPDATE: Algorithm successfully gives you the polynomial approximation

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Oct 23 19:16:45 2019
 
-@author: User
+@author: Alan Maldonado
 """
 
 """
@@ -14,18 +14,25 @@ Output: Polynomic Function
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
 #Default Variables
-
+x_lim = [-2, 3] #Graph Range
 
 #Default Functions
-def algorithmStart():
-    pass
+def algorithmStart(f, grad, x0):
+    f_der = f
+    p = 0
+    for k in range(grad+1):
+        p = p + (f_der.evalf(subs={'x': x0})/math.factorial(k))*(x-x0)**k
+        f_der = sp.diff(f_der, 'x')
+    p = sp.expand(p)
+    print(p)
 
-# Change
-x = sp.symbol('x') #X is the symbol.
+########### Change
+x = sp.Symbol('x') #X is the symbol.
 f = sp.exp(x) #Define the function
 
-poly_grad = 1
-center_p = 0
+poly_grad = 3 #Polynomial Grade
+center_p = 1 #Center Point
 algorithmStart(f, poly_grad, center_p)
