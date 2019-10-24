d85550577029593bc8d103c4fa32e90a0d66ce98
Alan Maldonado <business.alanm@gmail.com>
2019-10-23 19:49:05 -0500

    FEATURE: Created main.py and refactored the file into a functional component

---
+++ main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 23 19:16:45 2019
+
+@author: User
+"""
+# Libraries
+import numpy as np
+
+#Default Variables
+
+
+#Default Functions
+def algorithmStart():
+    pass
+
+# Change
+    
+algorithmStart()
